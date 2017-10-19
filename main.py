dc75287041164b61efdcf2badea530b2edb48f82
Marko Koron <koron.marko@gmail.com>
2017-10-19 21:28:03 +0200

    Added docstring

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+"""
+The program takes a xml file as input and gives a complete website
+with the appertaining web pages in the folder public_html as output.
+
+Example:
+
+    You can use the program as following:
+
+        python main input_file.xml
+
+"""
 from xml.sax import parse
+import sys
 import dispatcher
 import constructor
 
 if __name__ == '__main__':
-    parse('website.xml', constructor.WebsiteConstructor('public_html'))
+    parse(sys.argv[1], constructor.WebsiteConstructor('public_html'))
